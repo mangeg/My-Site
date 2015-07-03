da8ec40781feac50950ec37ee14f9455c1cce275
Magnus Gideryd <mgideryd@gmail.com>
2015-07-04 01:21:53 +0200

    work

---
+++ src/Site/App/Services/dashboard.dataservice.ts
@@ -0,0 +1,42 @@
+﻿module App.Services {
+
+    export interface IWidget {
+        sizeX: number;
+        sizeY: number;
+        colX: number;
+        colY: number;
+        title: string;
+        dashboardId: number;
+    }
+
+    export interface IDashboard {
+        id: number;
+        name: string;
+        widgets: Array<IWidget>;
+    }
+
+    export interface IDashboardDataService {
+        getDashboards(): ng.IPromise<Array<IDashboard>>;
+    }
+
+    class DashboardDataService implements IDashboardDataService {
+        static $inject: Array<string> = ["$http"];
+
+        constructor( private $http: ng.IHttpService ) {}
+
+        getDashboards(): ng.IPromise<Array<IDashboard>> {
+            return this.$http.get( "api/dashboard", {
+                    headers: {
+                        ContentType: "application/json"
+                    }
+                } )
+                .then( ( data ) => {
+                    return data.data;
+                } );
+        }
+    }
+
+    angular
+        .module( "app.services" )
+        .service( "app.services.dashboardService", DashboardDataService );
+}
--- src/Site/Controllers/DashboardController.cs
+++ src/Site/Controllers/DashboardController.cs
@@ -4,6 +4,10 @@ namespace Site.Controllers
     using Data;
     using Microsoft.AspNet.Mvc;
     using Microsoft.Data.Entity;
+    using Microsoft.Data.Entity.ChangeTracking;
+    using Microsoft.Data.Entity.Metadata;
+    using Microsoft.Data.Entity.Query;
+    using Remotion.Linq.Clauses;
 
     [Route( "api/dashboard" )]
     public class DashboardController : Controller
@@ -22,7 +26,7 @@ namespace Site.Controllers
         {
             return _db.Dashboards;
         }
-        [Route( "{id:int}" )]
+        [Route( "{id:int}", Name = "GetDashboard")]
         [HttpGet]
         public IActionResult GetDashboard( int id )
         {
@@ -46,6 +50,8 @@ namespace Site.Controllers
             _db.Add( dashboard );
             _db.SaveChanges();
 
+            Response.Headers.Append( "Location", Url.Link( "GetDashboard", new { id = dashboard.Id } ) );
+
             return new NoContentResult();
         }
         [Route("{id:int}")]
@@ -58,7 +64,7 @@ namespace Site.Controllers
                 return new HttpNotFoundResult();
             }
 
-            match.Name = dashboard.Name;
+            _db.Entry( match ).Update( dashboard );
             _db.SaveChanges();
 
             return new NoContentResult();
@@ -78,6 +84,8 @@ namespace Site.Controllers
 
             return new NoContentResult();
         }
+
+
         [Route( "{dashboardId:int}/widgets" )]
         [HttpGet]
         public IActionResult GetWidgets( int dashboardId )
@@ -88,11 +96,104 @@ namespace Site.Controllers
             }
             return new ObjectResult( _db.Widgets.Where( w => w.DashboardId == dashboardId ) );
         }
-        [Route( "widget/{widgetId:int}" )]
-        [HttpPut]
-        public void PutWidget( int widgetId, [FromBody] Widget widget )
+        [Route("widget/{id:int}", Name = "GetWidget")]
+        [HttpGet]
+        public IActionResult GetWidget( int id )
         {
-            Response.Headers.Add( "Location", new[] { "ddgdg" } );
+            var match = _db.Widgets.FirstOrDefault( w => w.Id == id );
+            if ( match == null )
+            {
+                return HttpNotFound();
+            }
+
+            return new ObjectResult( match );
+        }
+        [Route("widget")]
+        [HttpPost]
+        public IActionResult CreateWidget( [FromBody] Widget widget )
+        {
+            if ( widget.DashboardId <= 0 )
+            {
+                return new BadRequestResult();
+            }
+
+            return CreateWidget( widget.DashboardId, widget );
+        }
+        [Route("{widgetId:int}/widget")]
+        [HttpPost]
+        public IActionResult CreateWidget( int dashboardId, [FromBody] Widget widget )
+        {
+            if ( string.IsNullOrWhiteSpace( widget.Title ) )
+            {
+                return new BadRequestObjectResult( "Title is required" );
+            }
+
+            var dashboard = _db.Dashboards.FirstOrDefault( d => d.Id == dashboardId );
+            if ( dashboard == null )
+            {
+                return HttpNotFound( "Dashboard not found" );
+            }
+
+            widget.Id = 0;
+            widget.DashboardId = dashboardId;
+
+            _db.Add( widget );
+            _db.SaveChanges();
+
+            var location = Url.Link( "GetWidget", new { id = widget.Id } );
+            Response.Headers.Append( "Location", location );
+
+            return new NoContentResult();
+        }
+        [Route( "widget/{id:int}" )]
+        [HttpPut]
+        public IActionResult PutWidget( int id, [FromBody] Widget widget )
+        {
+            var match = _db.Widgets.FirstOrDefault( w => w.Id == id );
+            if ( match == null )
+            {
+                return new HttpNotFoundResult();
+            }
+
+            widget.Id = id;
+            _db.Entry( match ).Update( widget );
+            
+            _db.SaveChanges();
+
+            return new NoContentResult();
+        }
+        [Route("widget/{id:int}")]
+        [HttpDelete]
+        public IActionResult DeleteWidget( int id )
+        {
+            var match = _db.Widgets.FirstOrDefault( w => w.Id == id );
+            if ( match == null )
+            {
+                return new HttpNotFoundResult();
+            }
+
+            _db.Remove( match );
+            _db.SaveChanges();
+
+            return new NoContentResult();
+        }
+    }
+
+    public static class EntityExtensions
+    {
+        public static EntityEntry<T> Update<T>( this EntityEntry<T> entity, T other ) where T : class
+        {
+            var props = entity.Metadata.GetProperties();
+            foreach ( var prop in props )
+            {
+                if ( prop.StoreGeneratedPattern == StoreGeneratedPattern.Computed || prop.StoreGeneratedPattern == StoreGeneratedPattern.Identity )
+                    continue;
+
+                var val = entity.Context.Entry( other ).Property( prop.Name ).CurrentValue;
+                entity.Property( prop.Name ).CurrentValue = val;
+                entity.Property( prop.Name ).IsModified = true;
+            }
+            return entity;
         }
     }
 }
