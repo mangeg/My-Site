905778d53f8226d0d8317b2a14fbee8b46fe737a
Magnus Gideryd <mgideryd@gmail.com>
2015-07-14 01:36:55 +0200

    Return 404 middleware

--- src/Site/App/Services/services.module.ts
+++ src/Site/App/Services/services.module.ts
@@ -1,4 +1,4 @@
-﻿((): void => {
+﻿( (): void => {
     angular
         .module( ( "app.services" ), [] );
-})()
+} )();
--- src/Site/Startup.cs
+++ src/Site/Startup.cs
@@ -1,15 +1,25 @@
 ﻿namespace Site
 {
+    using System;
+    using System.Collections.Generic;
     using System.Linq;
+    using System.Runtime.InteropServices;
+    using System.Threading.Tasks;
     using Data;
     using Microsoft.AspNet.Builder;
     using Microsoft.AspNet.Diagnostics;
     using Microsoft.AspNet.Hosting;
+    using Microsoft.AspNet.Http;
     using Microsoft.AspNet.Mvc;
+    using Microsoft.AspNet.Routing;
+    using Microsoft.AspNet.Routing.Constraints;
+    using Microsoft.AspNet.Routing.Template;
+    using Microsoft.AspNet.StaticFiles;
     using Microsoft.Data.Entity;
     using Microsoft.Framework.Configuration;
     using Microsoft.Framework.DependencyInjection;
     using Microsoft.Framework.Logging;
+    using Microsoft.Framework.OptionsModel;
     using Microsoft.Framework.Runtime;
     using Newtonsoft.Json;
     using Newtonsoft.Json.Serialization;
@@ -64,15 +74,38 @@
             }
 
             app.UseStaticFiles();
+            app.UseMiddleware<NotFoundPathMiddleware>( "/js" );
+            app.UseMiddleware<NotFoundPathMiddleware>( "/content" );
 
             app.UseMvc(
                 routes =>
                 {
                     routes.MapRoute(
                         "default",
-                        "{*url}",
+                        @"{*url}",
                         new { controller = "Home", action = "Index" } );
                 } );
+        }
+    }
+
+    public class NotFoundPathMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly string _ignore;
+        public NotFoundPathMiddleware( RequestDelegate next, string ignore )
+        {
+            _next = next;
+            _ignore = ignore;
+        }
+
+        public Task Invoke( HttpContext context )
+        {
+            if ( context.Request.Path.StartsWithSegments( PathString.FromUriComponent( _ignore ) ) )
+            {
+                context.Response.StatusCode = 404;
+                return Task.FromResult( 0 );
+            }
+            return _next( context );
         }
     }
 }
